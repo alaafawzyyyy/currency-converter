06cce7093cfdfa705bb7e70aab8acee7f32d23b8
Alaa Shaaban <alaa.shaaban943@gmail.com>
2025-07-12 20:21:36 +0300

    Implement currency conversion functionality with loading state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const[amount,setAmount]=useState(1)
+  const[fromCurr,setFromCurr]=useState("USD")
+  const[toCurr,setToCurr]=useState("EUR")
+  const[converted,setConverted]=useState()
+  const[isLoading,setIsLoading]=useState("false")
+
+ useEffect(
+  function (){
+  async function convert(){
+    setIsLoading(true);
+    const res=await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`
+    );
+    const data=await res.json();
+    setConverted(data.rates[toCurr])
+    setIsLoading(false);
+  }
+
+    if (fromCurr === toCurr ) return setConverted(amount);
+    convert();
+ },
+  [amount,fromCurr,toCurr]
+);
+
+
   return (
   <div>
-    <input type="text" />
+    <input 
+      type="text"
+      value={amount}
+      onChange={(e)=>setAmount(Number(e.target.value))}
+      disabled={isLoading}
+    />
 
-    <select>
+    <select 
+        value={fromCurr}
+        onChange={(e) => setFromCurr(e.target.value)}
+        disabled={isLoading}
+    >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
     </select>
 
-    <select>
+    <select
+    value={toCurr}
+     onChange={(e)=> setToCurr(e.target.value)}
+    disabled={isLoading}
+    >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
     </select>
 
-    <p>OUTPUT</p>
+    <p>
+      {converted}  {toCurr}
+    </p>
   </div>
   );
 }
